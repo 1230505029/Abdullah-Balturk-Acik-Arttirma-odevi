16258c6c7fada817dde648270655ea8300655723
1230505029 <1230505029>
2023-12-24 21:43:10 +0300

    Initial Commit

---
+++ Açık arttırma ödevi/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Açık arttırma ödevi
+//
+//  Created by ABDULLAH on 24.12.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
